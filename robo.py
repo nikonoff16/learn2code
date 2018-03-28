564ebe79b781904b680dee3338dfe1c8d25b6009
Victor Osipov <37671082+nikonoff16@users.noreply.github.com>
2018-03-28 22:45:55 +0500

    Main peace of code

---
+++ robo.py
@@ -0,0 +1,59 @@
+#! python3
+# -*- coding: utf-8 -*-
+
+coders_count = input()
+index = int(coders_count[-1])
+#print('Индекс равен ' + str(index))
+token = int(coders_count)
+#print('Токен равен ' + str(token))
+control = 0
+#print(index)
+while token > control:
+#    if len(coders_count) > 4:
+#        print("Программистов очень много, не могу сосчитать корректно.")
+#    coders_count = str(coders_count)
+    if  len(coders_count) == 2:
+        if int(coders_count[-2:-1]) == 11 :
+            print(coders_count + ' программистов')
+        elif int(coders_count[-2:-1]) == 12:
+            print(coders_count + ' программистов')
+        elif int(coders_count[-2:-1]) == 13:
+            print(coders_count + ' программистов')
+        elif int(coders_count[-2:-1]) == 14:
+            print(coders_count + ' программистов')
+        elif int(coders_count[-2:-1]) == 15:
+            print(coders_count + ' программистов')
+        elif int(coders_count[-2:-1]) == 16:
+            print(coders_count + ' программистов')
+        elif int(coders_count[-2:-1]) == 17:
+            print(coders_count + ' программистов')
+        elif int(coders_count[-2:-1]) == 18:
+            print(coders_count + ' программистов')
+        elif int(coders_count[-2:-1]) == 19:
+            print(coders_count + ' программистов')
+        else:
+            if index == 1:
+                print(coders_count + ' программист')
+            elif index == 2:
+                print(coders_count + ' программиста')
+            elif index == 3:
+                print(coders_count + ' программиста')
+            elif index == 4:
+                print(coders_count + ' программиста')
+            else:
+                print(coders_count + " программистов")
+    else:
+        if index == 1:
+            print(coders_count + ' программист')
+        elif index == 2:
+            print(coders_count + ' программиста')
+        elif index == 3:
+            print(coders_count + ' программиста')
+        elif index == 4:
+            print(coders_count + ' программиста')
+        else:
+            print(coders_count + " программистов")
+    token -= 1
+    coders_count = token
+    coders_count = str(coders_count)
+    index = int(coders_count[-1])
