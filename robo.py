8d9cdeb2a5e184564b884ece68e1cac3b3fd47da
nikonoff16 <i.nikonoff16@gmail.com>
2018-03-29 00:17:45 +0500

    Almost done
    Нашел проблему - я неправильно делал срезы, и мои условия не выполнялись. Надо не забыть те наработки, что я делал в этом задании

--- robo.py
+++ robo.py
@@ -3,6 +3,10 @@
 
 coders_count = input()
 index = int(coders_count[-1])
+length = len(coders_count)
+if length > 1:
+    index_2 = int(coders_count[-2:])
+    print('Index 2 equals ' + str(index_2))
 #print('Индекс равен ' + str(index))
 #token = int(coders_count)
 #print('Токен равен ' + str(token))
@@ -12,24 +16,14 @@ index = int(coders_count[-1])
 #    if len(coders_count) > 4:
 #        print("Программистов очень много, не могу сосчитать корректно.")
 #    coders_count = str(coders_count)
-if  len(coders_count) == 2:
-    if int(coders_count) == 11 :
+if  length >= 2:
+    if index_2 == 11 :
         print(coders_count + ' программистов')
-    elif int(coders_count) == 12:
+    elif index_2 == 12:
         print(coders_count + ' программистов')
-    elif int(coders_count) == 13:
+    elif index_2 == 13:
         print(coders_count + ' программистов')
-    elif int(coders_count) == 14:
-        print(coders_count + ' программистов')
-    elif int(coders_count) == 15:
-        print(coders_count + ' программистов')
-    elif int(coders_count) == 16:
-        print(coders_count + ' программистов')
-    elif int(coders_count) == 17:
-        print(coders_count + ' программистов')
-    elif int(coders_count) == 18:
-        print(coders_count + ' программистов')
-    elif int(coders_count) == 19:
+    elif index_2 == 14:
         print(coders_count + ' программистов')
     else:
         if index == 1:
@@ -42,6 +36,26 @@ if  len(coders_count) == 2:
             print(coders_count + ' программиста')
         else:
             print(coders_count + " программистов")
+# if  length == 3:
+#     if index_2 == 11 :
+#         print(coders_count + ' программистов')
+#     elif index_2 == 12:
+#         print(coders_count + ' программистов')
+#     elif index_2 == 13:
+#         print(coders_count + ' программистов')
+#     elif index_2 == 14:
+#         print(coders_count + ' программистов')
+#     else:
+#         if index == 1:
+#             print(coders_count + ' программист')
+#         elif index == 2:
+#             print(coders_count + ' программиста')
+#         elif index == 3:
+#             print(coders_count + ' программиста')
+#         elif index == 4:
+#             print(coders_count + ' программиста')
+#         else:
+#             print(coders_count + " программистов")
 else:
     if index == 1:
         print(coders_count + ' программист')
@@ -53,6 +67,6 @@ else:
         print(coders_count + ' программиста')
     else:
         print(coders_count + " программистов")
-#    coders_count = token
-#    coders_count = str(coders_count)
-#    index = int(coders_count[-1])
+# #    coders_count = token
+# #    coders_count = str(coders_count)
+# #    index = int(coders_count[-1])
