11449cab8717a7d87b5da1b81ddecd6b130e9e8f
Victor Osipov <i.nikonoff16@gmail.com>
2018-04-08 21:39:44 +0500

    Создаю новый файл

---
+++ table.py
@@ -0,0 +1,17 @@
+#! python3
+# -*- coding: utf-8 -*-
+
+first = int(input())
+second = int(input())
+third = int(input())
+fourth = int(input())
+
+if first > second or third >fourth:
+    print("Fuck you")
+elif first > 10 or second > 10 or third > 10 of fourth > 10:
+    print("Fuck you")
+else:
+    for foo in range(first, second+1):
+        for egg in range(third, fourth+1):
+            print(foo * egg)
+print()
