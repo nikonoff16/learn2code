bc26ac4ec1445096600521c1c010d57108739385
Victor Osipov <i.nikonoff16@gmail.com>
2018-04-09 19:52:40 +0500

    That's working version of script. Congrats, send to master

--- table.py
+++ table.py
@@ -8,10 +8,15 @@ fourth = int(input())
 
 if first > second or third >fourth:
     print("Fuck you")
-elif first > 10 or second > 10 or third > 10 of fourth > 10:
+elif first > 10 or second > 10 or third > 10 or fourth > 10:
     print("Fuck you")
 else:
+    for x in range(third, fourth+1):
+        print('\t', x, end="")
+    print()
     for foo in range(first, second+1):
+        print(foo, end='\t')
         for egg in range(third, fourth+1):
-            print(foo * egg)
+            print(foo * egg, end='\t')
+        print()
 print()
