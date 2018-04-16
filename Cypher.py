9210bbddd1896e6167c1a8f20b5ba21e00e15954
Victor Osipov <i.nikonoff16@gmail.com>
2018-04-16 16:06:21 +0500

    First working

--- Cypher.py
+++ Cypher.py
@@ -3,48 +3,55 @@
 
 # создаю строку, которую нужно "зашифровать"
 target_str = input()
-# цельночисленная переменная, используемая для сравнения предыдущего символа в target_str 
-slice_count = -1
+
+# принудительно изменяем регистр на малый
+#target_str = target_str.lower()
+# цельночисленная переменная, используемая для итерирования цикла
+slice_count = 0
+# строка, в которую записывается вывод
 cyphered_str = "" 
-# строка, в которую записывается вывод /выше/
+# счетчик
+shiphr = 1
 
-# создаю цикл для посимвольного прохода строки слева на право
-for foo in target_str:
-# Создаю условие, которое отсеивает несколько символов в ряд, уже сжатых
-	if slice_count >= 0:
-		if cyphered_str[-1].isnumeric() == True :
-			print(cyphered_str[-1].isnumeric())
-			if foo == target_str[slice_count]:
-				continue
-#добавляю символ в указанную строку для последующего сохранния или изменения
-	cyphered_str = cyphered_str + foo
-	print(cyphered_str)
-
-	
-	
-	# исключаю ошибку алгоритма при проходе первого символа - программа выполняется, если slice_count не в начальнлй позиции
-	if slice_count >=0:
-		# создаю клон slice_count для просчета повторяющихся символов в следующем цикл
-		egg = slice_count
-		# создаю счетчик, запоминающий количество одинаковых си волов подряд с ненулевым значением
-		count = 0
-		# создаю цкл вычисляющий количество подряд идущих сиволов, направленный справа налево. 
+"""
+Первый цикл обрабатывает одиночные символы и недопускает ошибки вида:
+- единственный символ в строке
+- выход индекса строки за рамки
+"""
+while slice_count < len(target_str):
+	current_symbol = target_str[slice_count]
+	shiphr = 1
+	if slice_count == (len(target_str) - 1):
+		if len(target_str) == 1:
+			cyphered_str = cyphered_str + current_symbol + str(shiphr)
+			break
+		elif target_str[-2] == target_str[-1]:
+			break
+		else:
+			cyphered_str = cyphered_str + current_symbol + str(shiphr)
+			break
+# обработка одиночных символов
+	elif current_symbol != target_str[slice_count +1]:
+		cyphered_str = cyphered_str + current_symbol + str(shiphr)
+		slice_count += 1
+		continue
+	elif current_symbol == target_str[slice_count +1]:
+		cyphered_str = cyphered_str + current_symbol
+		shiphr = 1
+#Второй цикл обрабатывает последовательности одинаковых символов
+#Он также имеет условие, не допускающее выход за границы строки.
 		while True:
-			if foo == target_str[egg]:
-				count += 1
-				if egg < (len(target_str)-1):
-					egg += 1
-				else:
-					break
-			else:
+			if slice_count == (len(target_str) - 1):
+				cyphered_str = cyphered_str + str(shiphr)
 				break
-		if count > 1:
-			cyphered_str = cyphered_str + str(count)
+			elif current_symbol == target_str[slice_count +1]:
+				shiphr += 1
+				slice_count += 1
+			else:
+				cyphered_str = cyphered_str + str(shiphr)
+				slice_count += 1
+				break
 
-	
-		
-	slice_count += 1
-	print(slice_count)
 # По завершении цикла напечатать получившийся шифр
 print(cyphered_str)
 	
