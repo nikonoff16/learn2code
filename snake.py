f6b6b86fe0772645b2feaa5579a4431cfe9eae84
Victor Osipov <37671082+nikonoff16@users.noreply.github.com>
2018-05-02 18:39:46 +0500

    Partially works

--- snake.py
+++ snake.py
@@ -6,10 +6,40 @@ Creating a matrix and filling it with numbers of n_square ** 2.
 '''
 
 n_square = int(input())
-matrix = [[0]* n_square] * n_square
-snake = []
+matrix = [[0 for egg in range(n_square)] for foo in range(n_square)]
+matrix[0][0] = 1
+mark = 0
+count = 1
+# while count <= (n_square**2):
+for firstrow in range(n_square):
+    matrix[mark][firstrow] = count
+    count += 1
+    if firstrow == n_square - 1:
+        n_square -= 1
+        mark = firstrow
+        print('First fixed ordinate is ', mark)
 
+for row in range(n_square):
+    matrix[row+1][mark] = count
+    count += 1
+    if row == n_square:
+        mark = row
+        print('Second fixed ordinate is ', mark, 'row value is', row)
+        for row in matrix:
+            print(' '.join([str(elem) for elem in row]))
+
+
+
+# for row in range(n_square):
+#     matrix[mark][row+1] = count
+#     count += 1
+#     print('row value is', row)
+#
+#     if row == n_square - 1:
+#         n_square = 0
+#         mark = firstrow
+#         print('Third fixed ordinate is ', mark)
 
 # Making the Snake visible
-for row in snake:
+for row in matrix:
     print(' '.join([str(elem) for elem in row]))
