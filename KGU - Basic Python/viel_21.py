5296778ee1466824a0a3ded3018944e59eba09c9
Viktor Osipov <i.nikonoff16@gmail.com>
2018-11-09 12:49:35 +0500

    Final viel_21

--- KGU - Basic Python/viel_21.py
+++ KGU - Basic Python/viel_21.py
@@ -1,13 +1,29 @@
-countries = {'Russia', 'China', 'Korea'}
-Autos = {
-    'Toyota':{'Russia', 'Korea'}, 
-    'Toyamatokanava': set(), 
-    'Mazda': {'Russia', 'China', 'Korea'}
-    }
-for mark in Autos:
-    if Autos[mark] == countries:
-        print(mark, 'поставляется во все страны')
-    elif not Autos[mark]:
-        print(mark, 'не импортируется никуда')
+# countries = {'Russia', 'China', 'Korea'}
+# Autos = {
+#     'Toyota':{'Russia', 'Korea'}, 
+#     'Toyamatokanava': set(), 
+#     'Mazda': {'Russia', 'China', 'Korea'}
+    # }
+count = int(input('Какое количество автомобильных марок вы собираетесь внести: '))
+countries = set()
+autos = {}
+while count != 0:
+    auto_seria = input('Введите имя марки: ')
+    auto_import = input('Введите страны экспорта через пробел: ')
+    if auto_import == '':
+        autos[auto_seria] = set()
     else:
-        print(countries.intersection(Autos[mark]))
+        autos[auto_seria] = set(auto_import.split(' '))
+    countries = countries.union(autos[auto_seria])
+    count -= 1
+
+# print(countries, autos)
+for mark in autos:
+    if not autos[mark]:
+        print(mark, 'не поставляется никуда')
+    elif autos[mark] == countries:
+        print(mark, 'поставляется во все страны')
+    else:
+        where_imp = countries.intersection(autos[mark])
+        print(mark, 'поставляется в', ', '.join(list(where_imp)))
+    
